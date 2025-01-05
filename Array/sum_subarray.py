384c8457331179485fa613e873a86c2f3a223465
Anoop Vantagodi <Anoopvantagodi420@gmail.com>
2025-01-05 10:25:05 +0530

    added optimul approach method

--- Array/sum_subarray.py
+++ Array/sum_subarray.py
@@ -11,5 +11,19 @@ def brute_force(arr):
             current_sum += arr[j]
             max_sum = max(current_sum,max_sum)
     return max_sum
-print(brute_force(arr))
+# Kendes algorithm
+def optimul_approch(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+    for i in range(0,len(arr)):
+        current_sum += arr[i]
+        max_sum = max(max_sum,current_sum)
+        if current_sum < 0:
+            current_sum = 0
+    return max_sum   
+
+    
+# print(brute_force(arr))
+print(optimul_approch(arr))
+
 
